784d9e48017cb0852fdae37bda2e246f8757e0ac
Mikhail C <7thsamurai@mail.ru>
2018-02-27 16:46:25 +0300

    Реализация красивого принтера для json-фалйов

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,25 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    d = json.dumps(data, ensure_ascii=False, separators=(',', ':'), indent=2)
+    print(d)
 
 
 if __name__ == '__main__':
-    pass
+    while True:
+        path = input('Укажите путь до json файла ')
+
+        try:
+            data = load_data(path)
+            pretty_print_json(data)
+            break
+        except FileNotFoundError:
+            print('Чё-то нет такого файла.')
+        except json.decoder.JSONDecodeError:
+            print('Прости, но это я не могу распарсить.')

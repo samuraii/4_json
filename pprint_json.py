ef97149fa2c375ab03b7724be895fd448c4da719
Mikhail C <7thsamurai@mail.ru>
2018-03-01 12:28:30 +0300

    Правки по ревью
    1) Убрал лишние отступы
    2) Добавил обработку ошибки если файл не передан

--- pprint_json.py
+++ pprint_json.py
@@ -10,22 +10,22 @@ def load_data(filepath):
 
 def pretty_print_json(data_from_file):
     json_data = json.dumps(
-            data_from_file, 
-            ensure_ascii=False,
-            separators=(',', ':'), 
-            indent=2
+        data_from_file,
+        ensure_ascii=False,
+        separators=(',', ':'),
+        indent=2
     )
-    
     print(json_data)
 
 
 if __name__ == '__main__':
+    try:
         path_to_file = sys.argv[1]
-
-        try:
-            data_from_file = load_data(path_to_file)
-            pretty_print_json(data_from_file)
-        except FileNotFoundError:
-            print('Чё-то нет такого файла.')
-        except json.decoder.JSONDecodeError:
-            print('Прости, но это я не могу распарсить.')
+        data_from_file = load_data(path_to_file)
+        pretty_print_json(data_from_file)
+    except IndexError:
+        print('Для того чтобы заработало, нужно предать скрипту файл.')
+    except FileNotFoundError:
+        print('Чё-то нет такого файла.')
+    except json.decoder.JSONDecodeError:
+        print('Прости, но это я не могу распарсить.')
